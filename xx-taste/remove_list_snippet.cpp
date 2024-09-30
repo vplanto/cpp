71fd94dd24cfb192038c77ea4a697acbbe9849d3
v.platonov <vitaliy.platonov@playtech.com>
2024-09-30 11:40:15 +0300

    Add tasteful code example

---
+++ xx-taste/remove_list_snippet.cpp
@@ -0,0 +1,35 @@
+remove_list_entry(entry){
+	prev = NULL;
+	walk = head;
+	// Walk the list
+	
+	while (walk !=entry) {
+		prev = walk;
+		walk = walk->next;
+	}
+
+	//Remove the entry by updating the 
+	// head or the previous entry
+	
+	if (!prev)
+		head = entry->next;
+	else
+		prev->next = entry->next;
+}
+
+remove_list_entry_tasteful(entry)
+{
+	// The inderect pointer point to the 
+	// *address* of the thing we`ll update
+	
+	inderect = &head;
+
+	// Walk the list, looking for the thing that
+	// points to the entry we want to remove
+	
+	while ((*indirect) != entry) 
+		indirect = &(*inderect)->next;
+
+	// .. and just remove it
+	*inderect = entry->next;
+}
