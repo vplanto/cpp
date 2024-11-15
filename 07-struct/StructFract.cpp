cf971193c8e18e6a3ec6737281239a2d0b696c61
Vitaliy Platonov <vitaly.platonov@gmail.com>
2024-11-15 09:30:15 +0200

    Fixed inline

--- 07-struct/StructFract.cpp
+++ 07-struct/StructFract.cpp
@@ -2,26 +2,27 @@
 
 // Define the struct with two fields: num and denum
 typedef struct {
-    int num;
-    int denum;
+  int num;
+  int denum;
 } fraction;
 
 int main() {
-    // Declare a variable of type fraction
-    fraction pi;
+  // Declare a variable of type fraction
+  fraction pi;
 
-    // Assign values to the fields
-    pi.num = 7;
-    pi.denum = 22;
+  // Assign values to the fields
+  pi.num = 7;
+  pi.denum = 22;
 
-    // Print the initial values
-    printf("Initial values: num = %d, denum = %d\n", pi.num, pi.denum);
+  // Print the initial values
+  printf("Initial values: num = %d, denum = %d\n", pi.num, pi.denum);
 
-    // Manipulate the pointer to change the num field via the denum field's address
-    ((fraction*)&(pi.denum))->num = 12;
+  // Manipulate the pointer to change the num field via the denum field's
+  // address
+  ((fraction*)&(pi.denum))->num = 12;
 
-    // Print the modified values
-    printf("Modified values: num = %d, denum = %d\n", pi.num, pi.denum);
+  // Print the modified values
+  printf("Modified values: num = %d, denum = %d\n", pi.num, pi.denum);
 
-    return 0;
+  return 0;
 }
